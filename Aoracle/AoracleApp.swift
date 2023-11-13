b32bc3452daabb4b0bb2144a8dcb77f4dd039dfd
Justin Leishman <justin.leishman@firmaneue.com>
2023-11-13 07:22:51 -0500

    Initial Commit

---
+++ Aoracle/AoracleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AoracleApp.swift
+//  Aoracle
+//
+//  Created by Justin Leishman on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct AoracleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
