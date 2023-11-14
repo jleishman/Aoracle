3d616896c3947244f621a909dab8bb35f0355205
Justin Leishman <justin.leishman@firmaneue.com>
2023-11-13 19:40:18 -0500

    Recreated Project to Get Rid of Entitlement Problems

--- Aoracle/ContentView.swift
+++ Aoracle/ContentView.swift
@@ -8,17 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.openURL) private var openURL
+        
+    @StateObject private var model = ContentViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Spacer()
+        
+        if let percentage = model.predictionPercentage {
+            Text("\(percentage.formatted(.number.precision(.fractionLength(0))))% Correctly Predicted")
+                .font(.largeTitle)
+        } else {
+            Text("Keep tapping")
+                .font(.largeTitle)
         }
-        .padding()
-    }
+        
+        HStack {
+            Button(action: {
+                model.append(.blue)
+            }, label: {
+                RoundedRectangle(cornerRadius: 10, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.blue)
+            })
+            .buttonStyle(.borderless)
+            .focusable(false)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .keyboardShortcut(KeyEquivalent("b"), modifiers: [])
+            
+            Spacer()
+            
+            Button(action: {
+                model.append(.green)
+            }, label: {
+                RoundedRectangle(cornerRadius: 10, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.green)
+            })
+            .buttonStyle(.borderless)
+            .focusable(false)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .keyboardShortcut(KeyEquivalent("g"), modifiers: [])
+        }
+        .padding([.leading, .trailing, .bottom], 10)
+        
+        Button("About the Aaronson Oracle") {
+            openURL(URL(string: "https://duckduckgo.com/?q=aaronson+oracle")!)
+        }
+        .buttonStyle(.borderless)
+        .focusable(false)
+        
+        Spacer()
+    }    
 }
 
 #Preview {
     ContentView()
 }
+
---
+++ Aoracle/ContentViewModel.swift
@@ -0,0 +1,117 @@
+//
+//  ContentViewModel.swift
+//  Aoracle
+//
+//  Created by Justin Leishman on 11/13/23.
+//
+
+import Foundation
+import OSLog
+
+class ContentViewModel: ObservableObject {
+    let logger = Logger(subsystem: "Aoracle", category: "ContentViewModel")
+
+    enum Value: Hashable, CustomStringConvertible {
+        case blue
+        case green
+        
+        var description: String {
+            switch self {
+            case .blue: "B"
+            case .green: "G"
+            }
+        }
+    }
+    
+    @Published var values = [Value]()
+    @Published var histogram = [[Value] : [Value : Int]]()
+    
+    @Published var prediction: Value?
+    @Published var correctPredictions = 0
+    @Published var totalPredictions = 0
+    @Published var predictionPercentage: Double?
+    
+    let nGramSize = 5
+    let corpusSize = 20
+
+    func append(_ value: Value) {
+        logger.debug("Appending value: \(value)")
+        
+        values.append(value)
+        
+        logger.debug("values.count: \(self.values.count)")
+        
+        if prediction == value {
+            logger.debug("Prediction was correct, incrementing correctPredictions")
+            correctPredictions += 1
+            logger.debug("correctPredictions: \(self.correctPredictions)")
+        }
+
+        if values.count > nGramSize {
+            let previousNGram = Array(values[(values.endIndex - nGramSize - 1)..<(values.endIndex - 1)])
+
+            logger.debug("values: \(self.values)")
+            logger.debug("nGram: \(previousNGram)")
+            
+            update(previousNGram, value)
+            
+            if values.count > corpusSize {
+                let currentNGram = Array(values[(values.endIndex - nGramSize)..<values.endIndex])
+                
+                prediction = predict(currentNGram)
+                
+                logger.debug("Incrementing total predictions")
+                totalPredictions += 1
+                
+                logger.debug("correctPredictions: \(self.correctPredictions)")
+                logger.debug("totalPredictions: \(self.totalPredictions)")
+                
+                if totalPredictions > 0 {
+                    predictionPercentage = 100 * Double(correctPredictions) / Double(totalPredictions)
+                    logger.debug("predictionPercentage: \(self.predictionPercentage!)")
+                }
+            }
+        }
+    }
+    
+    private func update(_ nGram: [Value], _ next: Value) {
+        logger.debug("Updating histogram for \(nGram) with value: \(next)")
+        
+        var current = current(nGram)
+        
+        logger.debug("Current histogram for \(nGram): \(current)")
+        
+        current[next] = (current[next] ?? 0) + 1
+        
+        logger.debug("Updated histogram for \(nGram): \(current)")
+        
+        histogram[nGram] = current
+    }
+    
+    private func current(_ nGram: [Value]) -> [Value : Int] {
+        if let current = histogram[nGram] {
+            return current
+        }
+        
+        return [.blue : 0, .green: 0]
+    }
+    
+    private func predict(_ nGram: [Value]) -> Value {
+        logger.debug("Predicting next value for \(nGram)")
+
+        let current = current(nGram)
+        
+        logger.debug("Current histogram for \(nGram): \(current)")
+
+        if current[.blue] ?? 0 >= current[.green] ?? 0 {
+            logger.debug("Predicting blue for next value")
+
+            return .blue
+        }
+        
+        logger.debug("Predicting green for next value")
+
+        return .green
+    }
+}
+
